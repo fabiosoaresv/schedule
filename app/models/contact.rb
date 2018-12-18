31f76237d9d2c64974fdc277587f3135da89c5cd
Fabio Soares <fabio.soares@autoseg.com>
2018-12-17 22:01:32 -0200

    Update project

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,6 @@
 class Contact < ApplicationRecord
   belongs_to :kind
   has_one :address
-  has_many :phones
 
   accepts_nested_attributes_for :address
-  accepts_nested_attributes_for :phones, allow_destroy: true
-
-  validates :name, presence: true, length: { minimum: 3 }
-  validates :email, presence: true
 end
